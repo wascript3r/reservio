09901a9ef81dfcb34173dc5a684511b7e023f550
WaScript3r <wascript3r@gmail.com>
2022-09-19 12:49:56 +0300

    updated migration file

--- db/migrations/20220915140634_init.sql
+++ db/migrations/20220915140634_init.sql
@@ -5,15 +5,27 @@ CREATE TYPE role_name AS enum ('client', 'company', 'admin');
 
 CREATE TABLE users
 (
-    id       uuid DEFAULT gen_random_uuid() NOT NULL
+    id         uuid                     DEFAULT gen_random_uuid() NOT NULL
         PRIMARY KEY,
-    email    varchar(200)                   NOT NULL,
-    password varchar(70)                    NOT NULL,
-    role     role_name                      NOT NULL
+    email      varchar(200)                                       NOT NULL,
+    password   varchar(70)                                        NOT NULL,
+    role       role_name                                          NOT NULL,
+    created_at timestamp WITH TIME ZONE DEFAULT NOW()             NOT NULL
 );
 
 CREATE UNIQUE INDEX users_email_uindex
     ON users (LOWER(email::text));
+
+CREATE TABLE companies
+(
+    id          uuid                                   NOT NULL
+        PRIMARY KEY
+        REFERENCES users,
+    name        varchar(100)                           NOT NULL,
+    address     varchar(200)                           NOT NULL,
+    description text                                   NOT NULL,
+    created_at  timestamp WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
 
 
 -- migrate:down
