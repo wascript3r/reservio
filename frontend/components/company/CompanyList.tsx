2c9fd65be0edb4172289a3e5c9168f805179b77c
WaScript3r <wascript3r@gmail.com>
2022-10-18 21:11:54 +0300

    update

--- frontend/components/company/CompanyList.tsx
+++ frontend/components/company/CompanyList.tsx
@@ -2,6 +2,7 @@ import {useQuery} from "react-query";
 import axios from "axios";
 import Spinner from "../utils/Spinner";
 import Err from "../utils/Err";
+import Link from "next/link";
 
 const CompanyList = () => {
 	const {data, error, isLoading} = useQuery<any, Error>("companies", () => {
@@ -20,8 +21,8 @@ const CompanyList = () => {
 		<>
 			<h2 className="text-center my-5">Registered companies</h2>
 			<div className="row row-cols-1 row-cols-md-2 mb-3 text-center">
-				{data?.data.companies.map((company: any) => (
-					<div className="col">
+				{data?.data.companies.map((company: any, index: number) => (
+					<div className="col" key={index}>
 						<div className="card mb-4 rounded-3 shadow-sm border-primary">
 							<div className="card-header py-3 text-white bg-primary border-primary">
 								<h4 className="my-0 fw-normal">{company.name}</h4>
@@ -33,7 +34,10 @@ const CompanyList = () => {
 									<span>{company.address}</span>
 								</div>
 
-								<button type="button" className="w-100 btn btn-lg btn-light mt-3">View services</button>
+								<Link href={`/companies/${company.id}`}>
+									<button type="button" className="w-100 btn btn-lg btn-light mt-3">View services
+									</button>
+								</Link>
 							</div>
 						</div>
 					</div>
