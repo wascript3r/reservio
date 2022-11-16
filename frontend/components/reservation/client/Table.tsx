a26f59d51f7301bf76251bac0273def5917c826b
WaScript3r <wascript3r@gmail.com>
2022-11-16 22:23:35 +0200

    responsive tables

--- frontend/components/reservation/client/Table.tsx
+++ frontend/components/reservation/client/Table.tsx
@@ -2,21 +2,23 @@ import {Reservation} from './Reservation'
 
 export const Table = ({reservations}: { reservations: any[] }) => {
 	return (
-		<table className="table">
-			<thead>
-			<tr>
-				<th>Company</th>
-				<th>Address</th>
-				<th>Service</th>
-				<th>Date</th>
-				<th>Actions</th>
-			</tr>
-			</thead>
-			<tbody>
-			{reservations.map((reservation, index) => (
-				<Reservation reservation={reservation} key={index}/>
-			))}
-			</tbody>
-		</table>
+		<div className="table-responsive">
+			<table className="table">
+				<thead>
+				<tr>
+					<th>Company</th>
+					<th>Address</th>
+					<th>Service</th>
+					<th>Date</th>
+					<th>Actions</th>
+				</tr>
+				</thead>
+				<tbody>
+				{reservations.map((reservation, index) => (
+					<Reservation reservation={reservation} key={index}/>
+				))}
+				</tbody>
+			</table>
+		</div>
 	)
 }
